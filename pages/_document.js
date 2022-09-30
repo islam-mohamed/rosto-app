4558995830dd29a09fca707992852e9f0fe06c3b
Islam Mohamed <islam_abdelslam@yahoo.com>
2022-09-30 12:47:56 +0200

    fixing _document

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Cairo&family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
